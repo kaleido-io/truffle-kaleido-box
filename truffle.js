f630df77fd95f23c5f47978dda394eeeb9d497a6
Peter Broadhurst <peter.broadhurst@consensys.net>
2019-07-16 16:44:04 -0400

    Adopt new truffle version, and new compiler, with settings validated with Kaleido Geth/PoA

--- truffle.js
+++ truffle.js
@@ -3,29 +3,25 @@ const HTTPProviderRateLimitRetry = require('./lib/http-provider-rate-limit-retry
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  compilers: {
+	  solc: {evmVersion: 'homestead'}
+  },
   networks: {
     development: {
       provider: () => {
-        const appCred = 'yourappcred'; // from application credential widget
-        const connectionURL = 'nodeConnectionURL'; // without protocol (https://)
+        const appCred = 'zzifdotjy6:SwDT6cG0CjqrbmDs9KeSxFn_TiQYHub_AXpPaX1nKhU'; // from application credential widget
+        const connectionURL = 'zzgdffh067-zzf8kcbtal-rpc.dev2.photic.io'; // without protocol (https://)
         return new HTTPProviderRateLimitRetry(`https://${appCred}@${connectionURL}`, 100000);
       },
       network_id: "*", // Match any network id
       gasPrice: 0,
-      gas: 4500000,
+      gas: 4500000
       /* type: 'quorum' // Use this property for Quorum environments */
     },
   },
-  mocha: {
-    enableTimeouts: false,
-    before_timeout: 600000
-  },
-  compilers: {
-    solc: {
-      version: "0.5.8",
-      settings: {
-        evmVersion: "byzantium"
-      }
-    },
-  }
+mocha: {
+enableTimeouts: false,
+	bail: true,
+	timeout: 600000
+}
 };
