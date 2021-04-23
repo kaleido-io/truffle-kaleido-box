e5cab536a9ace3517c0b8e77d0da5c8d2d6b2b05
Peter Broadhurst <peter.broadhurst@kaleido.io>
2021-04-23 17:24:27 -0400

    Upgrade Truffle, and Web3

--- truffle-config.js
+++ truffle-config.js
@@ -22,9 +22,9 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.5.0",
+      version: "0.7.0",
       settings: {
-        evmVersion: "byzantium"
+        evmVersion: "constantinople"
       }
     },
   }
